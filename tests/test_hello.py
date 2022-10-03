888c02888ac19169805afa0920aaa73ca792d697
OvierI <111073908+OvierI@users.noreply.github.com>
2022-10-03 01:45:27 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -7,12 +7,12 @@ from desoper.SecondPip import solutionfinal
 
 class Test_hello(unittest.TestCase):
     def test__working1(self):
-        ls1=solutionfinal(5,9,0,30,400000)
-        self.assertEqual(12,ls1.shape[0], True)
+        ls1=solutionfinal(5,9,0,30,500000)
+        self.assertEqual(11,ls1.shape[0], True)
 
     def test__working2(self):
-        ls2=solutionfinal(6,9,0,30,400000)
-        self.assertEqual(141,ls2.shape[0], True)
+        ls2=solutionfinal(6,9,0,30,500000)
+        self.assertEqual(112,ls2.shape[0], True)
        
 
 if __name__ == '__main__':
